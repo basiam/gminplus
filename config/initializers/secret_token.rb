273ac03e3865b21306be8186d13c05355238c306
Basia Madej <basia@applicake.com>
2012-06-10 17:09:46 +0200

    gmin plus first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gmina::Application.config.secret_token = '5b8a0839ba152b0b1a5cc1ef1fb6aa6a25013944272741ded9ed28136b4f97e5e74954565ccb3ff527fd0c6ea63253980871a318e1f35a8cbb1a6d6749d602c0'
