bca209aad88eb6ed089e5aa07d1b4b8d842175a5
Basia Madej <basia@applicake.com>
2012-06-12 00:25:42 +0200

    form in progress

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,8 +1,16 @@
 jQuery(document).ready(function($) {
   $("#formlink").live("click", function(e){
-    e.preventDefault;
+    e.preventDefault();
     $("#page").fadeOut();
     $(".second").hide();
     $("#superform").fadeIn();
+  });
+  $(".first form").live("submit", function(e){
+    $(".first").fadeOut();
+    $(".second").fadeIn();
+  });
+  $(".second form").live("submit", function(e){
+    $(".first").fadeIn();
+    $("#page").fadeIn();
   });
 });
--- app/controllers/interests_controller.rb
+++ app/controllers/interests_controller.rb
@@ -7,16 +7,12 @@ class InterestsController < ApplicationController
 
   def create
     @interest = Interest.new(params[:interest])
-    puts request.inspect
     @interest.ip = request.remote_ip
 
-    respond_to do |format|
-      if @interest.save
-        redirect_to "/" and return
-      else
-        format.html { render action: "new" }
-        format.json { render json: @interest.errors, status: :unprocessable_entity }
-      end
+    if @interest.save
+      render json: @interest
+    else
+      render json: @interest.errors, status: :unprocessable_entity 
     end
   end
 end
