81de7a36b00bdc8bee5dfb6d91d0184fa85061d4
Basia Madej <basia@applicake.com>
2012-07-24 22:02:40 +0200

    error messages

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -6,11 +6,21 @@ jQuery(document).ready(function($) {
     $(".second").hide();
     $("#superform").fadeIn();
   });
+
   $("#ff").bind("ajax:success", function(event, data, status, xhr) {
     $("#fs").attr("action", "/interests/" + data.id);
     $(".first").hide();
     $(".second").fadeIn();
   });
+
+  $("#ff .button").bind("click", function(){
+   return validateErrors();
+  });
+
+  $("#fs .button").bind("click", function(){
+   return validateErrors();
+  });
+
   $("#fs").bind("ajax:success", function(event, data, status, xhr) {
     window.location = "/lastpage";
   });
@@ -20,45 +30,57 @@ jQuery(document).ready(function($) {
     $(".shy").show();
   });
 
+  setAutocomolete("#ac1")
+  setAutocomolete("#ac2")
 
-  $("#ac1").autocomplete({
-    source: function( request, response ) {
-      $.ajax({
-        url: "/communes", dataType: "json",
-        data: { query: request.term },
-        success: function(data) {
-          $("#ac1_b").val('')
-          return response($.map(data, function(item) {
-            return { label: item.name + " (" + item.desc + ")", value: item.id }
-          }));
-        }
-      });
-    },
-    minLength: 2,
-    select: function( event, ui ) {
-      $("#ac1").val(ui.item.label)
-      $("#ac1_b").val(ui.item.value)
-      return false;
-    }
-  });
-  $("#ac2").autocomplete({
-    source: function( request, response ) {
-      $.ajax({
-        url: "/communes", dataType: "json",
-        data: { query: request.term },
-        success: function(data) {
-          $("#ac2_b").val('')
-          return response($.map(data, function(item) {
-            return { label: item.name + " (" + item.desc + ")", value: item.id }
-          }));
-        }
-      });
-    },
-    minLength: 2,
-    select: function( event, ui ) {
-      $("#ac2").val(ui.item.label)
-      $("#ac2_b").val(ui.item.value)
-      return false;
-    }
-  });
+  if ('placeholder' in document.createElement('input') == false) {
+    $('[placeholder]').each( function(index, element){
+      var m = $(element).attr("placeholder")
+      element.value = m;
+      $(element).focus(function(){if(element.value==m ){element.value ='';} });
+      $(element).blur(function(){ if(element.value==''){element.value = m;} });
+    });
+  }
 });
+function validateErrors(){
+  $(".error").remove();
+  if ($(".required:visible[value=]").length > 0){
+    $(".required:visible[value=]").each(function(){
+      var left = "style='left:" + $(this).position().left +"px'";
+      var div = "<div class='error' " + left + ">Proszę wypełnij pole!</div>"
+      $(this).after(div)
+    });
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+function setAutocomolete(id){
+  $(id).autocomplete({
+    source: function( request, response ) {
+      $.ajax({
+        url: "/communes", dataType: "json",
+        data: { query: request.term },
+        success: function(data) {
+          $(id + "_b").val('')
+          $(".error").remove()
+          return response($.map(data, function(item) {
+            return { label: item.name + " (" + item.desc + ")", value: item.id }
+          }));
+        }
+      });
+    },
+    minLength: 2,
+    select: function( event, ui ) {
+      $(id).val(ui.item.label)
+      $(id + "_b").val(ui.item.value)
+      return false;
+    },
+    focus: function(event, ui) {
+      $(id).val(ui.item.label)
+      return false;
+    }
+
+  });
+}
